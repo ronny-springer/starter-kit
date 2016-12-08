9e5fc080dfd9b7b46366ff90d53ed79c7be9da7b
Ronny Springer <springer.ronny@gmail.com>
2016-12-08 13:17:55 +0100

    move index.html and styles into itsown module

--- web/src/entry.js
+++ web/src/entry.js
@@ -1,3 +1,4 @@
-require('./entry.css');
+require('./modules/app/app.css');
+require('file?name=[name].[ext]!./modules/app/index.html');
 
 console.log('Hello world.');
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
-var debug = process.env.NODE_ENV !== "production";
+const debug = process.env.NODE_ENV !== "production";
+const WEBDIR = './web';
+
 var webpack = require('webpack');
+var path = require("path");
+
 
 module.exports = {
-    context: __dirname + '\\web',
+    context: path.resolve(__dirname, WEBDIR),
     devtool: debug ? 'inline-sourcemap' : null,
-    entry: './src/entry.js',
-    output: {
-        path: __dirname + '\\web\\dist',
-        filename: 'bundle.min.js'
+    entry: {
+        app: './src/entry.js',
     },
+    output: {
+        path: path.join(__dirname, WEBDIR, './dist'),
+        filename: '[name].min.js'
+    },    
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style-loader!css-loader' }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, WEBDIR, './dist'),
+        inline: true
     },  
     plugins: debug ? [] : [
+        new HotModuleReplacementPlugin(),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
     ],
+    resolve: {
+        extensions: ['', '.js', '.json']
+    },
+    watch: true
 };
